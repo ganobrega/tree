783887594a51ba27480e896b3b3d0648a1c1953b
Gabriel Nobrega <gnobrega.contato@gmail.com>
2019-03-26 02:34:04 -0300

    Implementation of tailwind

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import '@/assets/css/tailwind.css'
+
+import Vue from 'vue'
+import App from './components/App.vue'
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
